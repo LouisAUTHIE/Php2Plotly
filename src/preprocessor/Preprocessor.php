76bfd8ba02aea8e9cec7e87f07e667c35f74f595
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2024-06-28 19:42:50 +0200

    add pearson correlation

--- src/preprocessor/Preprocessor.php
+++ src/preprocessor/Preprocessor.php
@@ -2,6 +2,33 @@
 namespace Php2plotly\preprocessor;
 
 class Preprocessor{
+    public static function pearsonCorrelation(array $x, array $y):float{
+        $n = count($x);
+        $sumX = array_sum($x);
+        $sumY = array_sum($y);
+        $sumX2 = 0;
+        $sumY2 = 0;
+        $sumXY = 0;
+        for($i = 0; $i < $n; $i++){
+            $sumX2 += $x[$i] * $x[$i];
+            $sumY2 += $y[$i] * $y[$i];
+            $sumXY += $x[$i] * $y[$i];
+        }
+        return ($n * $sumXY - $sumX * $sumY) / sqrt(($n * $sumX2 - $sumX * $sumX) * ($n * $sumY2 - $sumY * $sumY));
+    }
+
+    public static function buildArrayForPearsonCollerationMatrix(array $data):array{
+        $n = count($data);
+        $matrix = [];
+        for($i = 0; $i < $n; $i++){
+            $matrix[] = [];
+            for($j = 0; $j < $n; $j++){
+                $matrix[$i][] = self::pearsonCorrelation($data[$i], $data[$j]);
+            }
+        }
+        return $matrix;
+    }
+
     public static function countNumberByInterval(array $data, int $intervalCount):array{
         $values = [];
         $intervalLabels = [];
